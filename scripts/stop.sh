c487b52e7e7202bdae5b024b308bd8a8b90ddf8b
leejuno <zhqkfxm0127@naver.com>
2024-06-01 17:58:32 +0900

    fix: TIME_NOW 구하는 수식 수정2

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -7,15 +7,15 @@ JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 # 각종 로그 경로
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-TIME_NOW=$(date + %c)
+TIME_NOW=$(date +'%Y-%m-%d %H:%M:%S')
 
 # 현재 구동중인 애플리케이션의 PID 를 확인
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
 # 프로세스가 켜져있으면 종료시켜주기
 if [ -z $CURRENT_PID ]; then # PID 가 없다면
-    echo "$TIME_NOW > 실행중인 애플리케이션이 없습니다." >> $DEPLOY_LOG
+    echo "[$TIME_NOW] > 실행중인 애플리케이션이 없습니다." >> $DEPLOY_LOG
 else
-    echo "$TIME_NOW > 실행중인 애플리케이션을 종료합니다. PID = $CURRENT_PID" >> $DEPLOY_LOG
+    echo "[$TIME_NOW] > 실행중인 애플리케이션을 종료합니다. PID = $CURRENT_PID" >> $DEPLOY_LOG
     kill -15 $CURRENT_PID
 fi
